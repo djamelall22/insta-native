0da19bbf019832d45a27a69d79ae2677575f5247
djamelall22 <alphamodeling.contact@gmail.com>
2023-01-24 21:37:06 +0100

    first commit

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+
+const App = () => {
+  return (
+    <View>
+      <Text>App</Text>
+      <AntDesign name="phone" size={99} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
